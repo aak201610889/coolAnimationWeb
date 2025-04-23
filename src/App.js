a60817e3534ba5115f7840e53b595a387e151fad
ayham-kattan <aak201610885@gmail.com>
2025-04-23 10:06:16 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { useState } from "react";
-import Product from "./Product";
-import Selected from "./Selected";
-
-
 function App() {
-  const [selectedItem, setselectedItem] = useState([]);
   return (
-    <div className="App">
-      <div className="product">
-        <Product
-          selectedItem={selectedItem}
-          setselectedItem={setselectedItem}
-        />
-      </div>
-      <div className="selected">
-        <Selected  selectedItem={selectedItem}/>
-      </div>
+    <div className="App bg-blue-400">
+ii
     </div>
   );
 }
